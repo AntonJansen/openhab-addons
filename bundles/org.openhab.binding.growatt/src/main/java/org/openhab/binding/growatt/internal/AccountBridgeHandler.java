ae680c89651d4dffcb79a137a9f5b588f506ff6a
anton <anton@anton-Virtual-Machine>
2020-06-30 10:32:07 +0200

    Busy trying to get the libraries working. Checking setup of Eclipse now.

--- bundles/org.openhab.binding.growatt/src/main/java/org/openhab/binding/growatt/internal/AccountBridgeHandler.java
+++ bundles/org.openhab.binding.growatt/src/main/java/org/openhab/binding/growatt/internal/AccountBridgeHandler.java
@@ -1,8 +1,21 @@
 package org.openhab.binding.growatt.internal;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.smarthome.config.core.status.ConfigStatusMessage;
 import org.eclipse.smarthome.core.thing.Bridge;
@@ -22,30 +35,75 @@ public class AccountBridgeHandler extends ConfigStatusBridgeHandler {
         super(bridge);
     }
 
+    @SuppressWarnings("null")
     @Override
     public void initialize() {
         logger.debug("Initializing account bridge handler.");
         accountBridgeConfig = getConfigAs(AccountBridgeConfig.class);
         updateStatus(ThingStatus.UNKNOWN);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            logger.error("Cannot sleep", e);
-        }
-        updateStatus(ThingStatus.OFFLINE);
+
+        scheduler.execute(() -> {
+            boolean thingReachable = false; // <background task with long running initialization here>
+            // Make a login attempt to see if defined account and server are working.
+            // when done do:
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            try {
+                HttpPost httpPost = new HttpPost("https://server.growatt.com/LoginAPI.do");
+                List<NameValuePair> nvps = new ArrayList<>();
+                nvps.add(new BasicNameValuePair("userName", "AntonJansen"));
+                nvps.add(new BasicNameValuePair("password", "1a36591bceec49c832c79e27cd7e8b73"));
+                httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+                CloseableHttpResponse response = httpclient.execute(httpPost);
+
+                try {
+                    logger.debug(response.getStatusLine().toString());
+                    HttpEntity entity2 = response.getEntity();
+                    // do something useful with the response body
+                    // and ensure it is fully consumed
+                    EntityUtils.consume(entity2);
+                } catch (IOException e) {
+                    logger.error("Unexpected io exception while logging in.", e);
+                } finally {
+                    response.close();
+                }
+
+                if (thingReachable) {
+                    updateStatus(ThingStatus.ONLINE);
+                } else {
+                    updateStatus(ThingStatus.OFFLINE);
+                }
+
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+
+            } finally {
+                try {
+                    httpclient.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
     public Collection<@NonNull ConfigStatusMessage> getConfigStatus() {
+        logger.debug("getConfigStatus()");
         Collection<ConfigStatusMessage> configStatusMessages;
+        configStatusMessages = Collections.emptyList();
+
         if (accountBridgeConfig.getPassword() == "password") {
-            configStatusMessages = Collections.singletonList(ConfigStatusMessage.Builder.error("password")
+            configStatusMessages.add(ConfigStatusMessage.Builder.error("password")
                     .withMessageKeySuffix("Password not defined.").build());
-        }
-        if (accountBridgeConfig.getUsername() == "user") {
-            configStatusMessages = Collections.singletonList(ConfigStatusMessage.Builder.error("username")
+        } else if (accountBridgeConfig.getUsername() == "user") {
+            configStatusMessages.add(ConfigStatusMessage.Builder.error("username")
                     .withMessageKeySuffix("Username not defined.").build());
+        } else if (accountBridgeConfig.getServer().isBlank()) {
+            configStatusMessages.add(ConfigStatusMessage.Builder.error("server name")
+                    .withMessageKeySuffix("Server name hosting API not defined.").build());
         } else {
             configStatusMessages = Collections.emptyList();
         }
@@ -56,6 +114,7 @@ public class AccountBridgeHandler extends ConfigStatusBridgeHandler {
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
         // TODO Auto-generated method stub
+        logger.debug("Handle command on channel: " + channelUID + " with command: " + command);
 
     }
 
